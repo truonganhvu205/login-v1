8568350e781c33b71d1bc532c90142b30ebc49c9
Vu Truong Anh <truonganhvu205@gmail.com>
2025-10-07 11:08:15 +0700

    login & register & logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-
 const authController = require('../app/controllers/AuthController')
 
 router.post('/logout', authController.logout)
